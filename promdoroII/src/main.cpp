fec444d41c46489db32b93799b9cdaa450fd7797
Szabó Zoltán <zoltan.szabo@enet.hu>
2020-02-03 20:54:12 +0100

    Add WebAPI

--- promdoroII/src/main.cpp
+++ promdoroII/src/main.cpp
@@ -5,8 +5,13 @@
 #include <ArduinoOTA.h>
 
 #include <TM1637Display.h>
-#include <Adafruit_NeoPixel.h>
+// #include <Adafruit_NeoPixel.h>
+#include <NeoPixelBus.h>
 #include <RemoteDebug.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncJson.h>
+#include <ArduinoJson.h>
 
 const int CLK = 3;       //Narancs //Set the CLK pin connection to the display
 const int DIO = 13;      //Citrom//Set the DIO pin connection to the display
@@ -35,10 +40,36 @@ int resolution = 8;
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, ledRing, NEO_GRB + NEO_KHZ800);
+// Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, ledRing, NEO_GRB + NEO_KHZ800);
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(16, ledRing);
 TM1637Display display(CLK, DIO); //set up the 4-Digit Display.
 RemoteDebug Debug;
+AsyncWebServer server(80);
 
+AsyncCallbackJsonWebHandler *handler = new AsyncCallbackJsonWebHandler("/rest/endpoint", [](AsyncWebServerRequest *request, JsonVariant &json) {
+  JsonObject jsonObj = json.as<JsonObject>();
+  RgbColor color(jsonObj["R"].as<int>(), jsonObj["G"].as<int>(), jsonObj["B"].as<int>());
+  debugD("R: %u, G: %u, B: %u", color.R, color.G, color.B);
+  // int index = jsonObj["I"].as<int>();
+  for (uint16_t i = 0; i < strip.PixelCount() + 4; i++)
+  {
+    strip.SetPixelColor(i, color);
+  }
+  //strip(jsonObj["Br"].as<int>());
+  strip.Show();
+  request->send(200, "text/plain", "Hello");
+});
+
+static void chase(RgbColor c)
+{
+  for (uint16_t i = 0; i < strip.PixelCount() + 4; i++)
+  {
+    strip.SetPixelColor(i, c);     // Draw new pixel
+    strip.SetPixelColor(i - 4, 0); // Erase pixel a few steps back
+    strip.Show();
+    delay(250);
+  }
+}
 void setup()
 {
   // put your setup code here, to run once:
@@ -52,13 +83,14 @@ void setup()
     delay(5000);
     ESP.restart();
   }
-  MDNS.addService("telnet", "tcp", 23);
-  
-	Debug.begin(HOST_NAME); // Initialize the WiFi server
+
+  Debug.begin(HOST_NAME);         // Initialize the WiFi server
   Debug.setResetCmdEnabled(true); // Enable the reset command
 
-	Debug.showProfiler(true); // Profiler (Good to measure times, to optimize codes)
-	Debug.showColors(true); // Colors
+  Debug.showProfiler(true); // Profiler (Good to measure times, to optimize codes)
+  Debug.showColors(true);   // Colors
+
+  debugD("Start debug");
   // Port defaults to 3232
   // ArduinoOTA.setPort(3232);
 
@@ -104,6 +136,7 @@ void setup()
       });
 
   ArduinoOTA.begin();
+  MDNS.addService("telnet", "tcp", 23);
   Serial.println("Ready");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
@@ -113,8 +146,12 @@ void setup()
   ledcSetup(channel, freq, resolution);
   ledcAttachPin(buzzer, channel);
 
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  strip.Begin();
+  strip.ClearTo(RgbColor(0, 0, 0)); // Initialize all pixels to 'off'
+  strip.SetPixelColor(0, RgbColor(128, 0, 0));
+  strip.SetPixelColor(1, RgbColor(0, 128, 0));
+  strip.SetPixelColor(2, RgbColor(0, 0, 128));
+  strip.Show();
 
   pinMode(greenLed, OUTPUT);
   pinMode(redLed, OUTPUT);
@@ -123,11 +160,47 @@ void setup()
   digitalWrite(greenLed, HIGH);
   digitalWrite(redLed, HIGH);
   display.setBrightness(0x0a); //set the diplay to maximum brightness
+
+  server.addHandler(handler);
+  server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request) {
+    String message;
+    if (request->hasParam("message", true))
+    {
+      message = request->getParam("message", true)->value();
+      debugD(" %02u", message);
+      strip.SetPixelColor(0, message.toInt());
+      strip.Show();
+    }
+    else
+    {
+      message = "No message sent";
+    }
+    request->send(200, "text/plain", "Hello, POST: " + message);
+  });
+  server.onNotFound([](AsyncWebServerRequest *request) {
+    if (request->method() == HTTP_OPTIONS)
+    {
+      request->send(200);
+    }
+    else
+    {
+      request->send(404);
+    }
+  });
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Headers", "*");
+  server.begin();
+
+  chase(RgbColor(255, 0, 0)); // Red
+  chase(RgbColor(0, 255, 0)); // Green
+  chase(RgbColor(0, 0, 255)); // Blue
 }
 
 void loop()
 {
   ArduinoOTA.handle();
   Debug.handle();
-  debugD("* This is a message of debug level DEBUG");
+  // debugD("* This is a message of debug level DEBUG");
+  debugD("loop");
+  delay(100);
 }
